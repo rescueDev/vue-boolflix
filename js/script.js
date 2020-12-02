500755c4a037da20765407a56f2cb635a80b8122
Salvatore Borgia <sa.borgia3@gmail.com>
2020-12-02 15:47:32 +0100

    milestone 1 , style raw

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//Boolflix
+//creo una copia di Netflix, parto creando un api-key dal sito di themoviedb.org e interrogo con axios l'api
+
+//creo variabile con il valore dell'api
+const movieDb =
+  "https://api.themoviedb.org/3/search/movie?api_key=a7c4877ed2ef5089283e2a5845b4c723&query=";
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    searchInput: "",
+    moviesArray: [],
+  },
+  methods: {
+    searchFilm() {
+      axios.get(movieDb + this.searchInput).then((response) => {
+        const results = response.data.results;
+        console.log(results);
+        this.moviesArray = results;
+        console.log(this.moviesArray);
+      });
+    },
+  },
+  mounted() {},
+  computed: {},
+});
