7fc907116e2dc64f803df97fad0628db0aabeae1
Salvatore Borgia <sa.borgia3@gmail.com>
2020-12-05 12:14:53 +0100

    refactoring generale + movies e tv popular on mounted

--- js/script.js
+++ js/script.js
@@ -1,20 +1,31 @@
 //Boolflix
 //creo una copia di Netflix, parto creando un api-key dal sito di themoviedb.org e interrogo con axios l'api
 
-//creo variabile con il valore dell'api movies
-const movieDb =
-  "https://api.themoviedb.org/3/search/movie?api_key=a7c4877ed2ef5089283e2a5845b4c723&query=";
+//creo variabile con il valore dell'api movies da cercare
+const movieDb = "https://api.themoviedb.org/3/search/movie";
+
+//creo variabile con valore dell'api dei film piu recenti
+const popularMovies = "https://api.themoviedb.org/3/movie/popular";
 
 //creo variabile con il valore dell'api shows
-const showsDb =
-  "https://api.themoviedb.org/3/search/tv?api_key=a7c4877ed2ef5089283e2a5845b4c723&query=";
+const showsDb = "https://api.themoviedb.org/3/search/tv";
+
+//creo variabile con il valore dell'api shows piu recenti
+const popularShows = "https://api.themoviedb.org/3/tv/popular";
 
 var app = new Vue({
   el: "#app",
   data: {
     searchInput: "",
+    apiKey: "a7c4877ed2ef5089283e2a5845b4c723",
     moviesArray: [],
+    popularMovies: [],
     showsArray: [],
+    popularShows: [],
+    actorsMovie: [],
+    actorsShows: [],
+    moviesGenres: [],
+    showsGenres: [],
     languageFlag: [
       "img/italy.png",
       "img/united-kingdom.png",
@@ -32,40 +43,74 @@ var app = new Vue({
     indexShows: "",
     showFilmSection: false,
     showShowsSection: false,
+    popShowsVisible: true,
+    popMoviesVisible: true,
   },
   methods: {
     //funzione che chiama dall'api sia i film che gli shows
     searchFilm() {
+      this.popMoviesVisible = false;
+      this.popShowsVisible = false;
       if (this.searchInput !== "") {
-        axios.get(movieDb + this.searchInput).then((response) => {
-          const results = response.data.results;
-          //console.log(results);
-          this.moviesArray = results;
-          this.moviesArray.forEach((element) => {
-            element.hover = false;
+        axios
+          .get(movieDb, {
+            params: { api_key: this.apiKey, query: this.searchInput },
+          })
+          .then((response) => {
+            const results = response.data.results;
+            //console.log(results);
+            this.moviesArray = results;
+            this.moviesArray.forEach((element) => {
+              element.hover = false;
+            });
+            //console.log(this.moviesArray);
+            this.searchInput = "";
+            //controllo se l'array ha elementi, se si allora faccio apparire l'intestazione della sezione
+            if (this.moviesArray.length > 0) {
+              this.showFilmSection = true;
+            } else {
+              this.showFilmSection = false;
+            }
           });
-          //console.log(this.moviesArray);
-          this.searchInput = "";
-          //controllo se l'array ha elementi, se si allora faccio apparire l'intestazione della sezione
-          if (this.moviesArray.length > 0) {
-            this.showFilmSection = true;
-          }
-        });
-        axios.get(showsDb + this.searchInput).then((response) => {
-          const results = response.data.results;
-          //console.log(results);
-          this.showsArray = results;
-          this.showsArray.forEach((element) => {
-            element.hover = false;
+        axios
+          .get(showsDb, {
+            params: { api_key: this.apiKey, query: this.searchInput },
+          })
+          .then((response) => {
+            const results = response.data.results;
+            //console.log(results);
+            this.showsArray = results;
+            this.showsArray.forEach((element) => {
+              element.hover = false;
+            });
+            //console.log(this.showsArray);
+            //controllo se l'array ha elementi, se si allora faccio apparire l'intestazione della sezione
+            if (this.showsArray.length > 0) {
+              this.showShowsSection = true;
+            } else {
+              this.showShowsSection = false;
+            }
           });
-          //console.log(this.showsArray);
-          //controllo se l'array ha elementi, se si allora faccio apparire l'intestazione della sezione
-          if (this.showsArray.length > 0) {
-            this.showShowsSection = true;
-          }
-        });
       }
     },
+    // actorsDisplay(id) {
+    //   axios
+    //     .get(
+    //       "https://api.themoviedb.org/3/movie/" +
+    //         id +
+    //         "?api_key=" +
+    //         this.apiKey +
+    //         "&append_to_response=credits"
+    //     )
+    //     .then((risp) => {
+    //       // vogliamo solo i primi 5 attori quindi pushamo nell'array i primi 5 risultati
+    //       for (let i = 0; i < 5; i++) {
+    //         this.actorsMovies.push(risp.data.credits.cast[i]);
+    //       }
+    //       this.moviesGenres = risp.data.genres;
+    //       console.log(this.genreMovies);
+    //     });
+    // },
     //funzione per contrallare che bandiera assegnare in base alla lingua
     whatFlag: function (movie) {
       if (movie.original_language === "en") {
@@ -95,26 +140,39 @@ var app = new Vue({
     esciHover(movie, indice) {
       this.indexMovies = indice;
       movie.hover = !movie.hover;
-      setTimeout(() => {
-        this.indexMovies = "";
-      }, 1000);
-
-      console.log(movie.hover);
+      this.indexMovies = "";
     },
     //funzione per mostrare all'hover i dettagli show
     hoverShow(show, indice) {
       this.indexShows = indice;
-      movie.hover = !movie.hover;
+      show.hover = !show.hover;
     },
     //funzione per uscire dall'hover show
     leaveShow(show, indice) {
       this.indexShows = indice;
       show.hover = !show.hover;
-      setTimeout(() => {
-        this.indexShows = "";
-      }, 1000);
+      this.indexShows = "";
     },
   },
-  mounted() {},
+  mounted() {
+    axios
+      .get(popularMovies, {
+        params: { api_key: this.apiKey, language: "en-US" },
+      })
+      .then((response) => {
+        const results = response.data.results;
+        this.popularMovies = results;
+        console.log(this.popularMovies);
+      });
+    axios
+      .get(popularShows, {
+        params: { api_key: this.apiKey, language: "en-US" },
+      })
+      .then((response) => {
+        const results = response.data.results;
+        this.popularShows = results;
+        console.log(this.popularShows);
+      });
+  },
   computed: {},
 });
